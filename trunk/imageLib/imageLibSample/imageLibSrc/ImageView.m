c421bf92ddd3812ab976ed1ac85a274336bcd095
frank.gongpengjun <frank.gongpengjun@ee074b0a-212c-11de-9edc-3f40a397ab60>
2009-04-05 10:17:09 +0000

    add sub-project 'imageLibSample' and it works as a normal app

---
+++ trunk/imageLib/imageLibSample/imageLibSrc/ImageView.m
@@ -0,0 +1,65 @@
+//
+//  ImageView.m
+//
+
+#import "ImageView.h"
+#import "UIImage+Extension.h"
+
+@implementation ImageView
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+		CGPoint position = self.center;
+		
+		UIImageView * imgView = [[UIImageView alloc] initWithImage:[UIImage imageWithDataNamed:@"Apple_Logo.gif"]];
+		[self addSubview:imgView];
+		position.y = 80.0;
+		imgView.center = position;
+		
+		UILabel* imgLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+		imgLabel.textAlignment = UITextAlignmentCenter;
+		imgLabel.backgroundColor = [UIColor clearColor];
+		imgLabel.text = @"GIF";
+		position.y += imgView.bounds.size.height/2.0;
+		imgLabel.frame = CGRectMake(0,position.y,320.0,30.0);
+		[self addSubview:imgLabel];
+		
+		[imgLabel release];
+		[imgView release];
+		
+		imgView = [[UIImageView alloc] initWithImage:[UIImage imageWithDataNamed:@"Apple_Logo.png"]];
+		[self addSubview:imgView];
+		position.y = 200.0;
+		imgView.center = position;
+		
+		imgLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+		imgLabel.textAlignment = UITextAlignmentCenter;
+		imgLabel.backgroundColor = [UIColor clearColor];
+		imgLabel.text = @"PNG";
+		position.y += imgView.bounds.size.height/2.0;
+		imgLabel.frame = CGRectMake(0,position.y,320.0,30.0);
+		[self addSubview:imgLabel];
+		
+		[imgLabel release];
+		[imgView release];
+		
+		imgView = [[UIImageView alloc] initWithImage:[UIImage imageWithDataNamed:@"Apple_Logo.jpg"]];
+		[self addSubview:imgView];
+		position.y = 300.0;
+		imgView.center = position;
+		
+		imgLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+		imgLabel.textAlignment = UITextAlignmentCenter;
+		imgLabel.backgroundColor = [UIColor clearColor];
+		imgLabel.text = @"JPG";
+		position.y += imgView.bounds.size.height/2.0;
+		imgLabel.frame = CGRectMake(0,position.y,320.0,30.0);
+		[self addSubview:imgLabel];
+		
+		[imgLabel release];
+		[imgView release];
+    }
+    return self;
+}
+
+@end
