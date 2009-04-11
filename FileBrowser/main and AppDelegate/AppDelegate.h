8dc89bb458f97a6007a0972028f624fe884b242d
frank.gongpengjun <frank.gongpengjun@ee074b0a-212c-11de-9edc-3f40a397ab60>
2009-04-11 16:18:11 +0000

    add FileManager project

---
+++ FileBrowser/main and AppDelegate/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//
+
+#import <UIKit/UIKit.h>
+
+@class FileBrowserViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow					*window;
+	FileBrowserViewController	*rootViewController;
+	UINavigationController		*navigationController;
+}
+
+@property (nonatomic, retain) UIWindow					*window;
+@property (nonatomic, retain) UINavigationController	*navigationController;
+@property (nonatomic, retain) FileBrowserViewController	*rootViewController;
+
+@end
+
