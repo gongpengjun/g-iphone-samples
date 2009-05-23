dbcaa2306cfeecf99ddf7cd07564dbe681dc7789
frank.gongpengjun <frank.gongpengjun@ee074b0a-212c-11de-9edc-3f40a397ab60>
2009-05-23 03:05:01 +0000

    differ sent/received message

--- p2p/p2pChat/PeeringService/F1MessageItem.h
+++ p2p/p2pChat/PeeringService/F1MessageItem.h
@@ -9,13 +9,21 @@
 #import <Foundation/Foundation.h>
 #import "F1Item.h"
 
+enum {
+    F1MessageItemDirectionSent		= 0,
+    F1MessageItemDirectionReceived	= 1 << 0
+};
+typedef NSUInteger F1MessageItemDirection;
+
 @interface F1MessageItem : F1Item
 {
 	@private
 	NSString *message;
+	F1MessageItemDirection direction;
 }
 
 @property (nonatomic,copy) NSString *message;
+@property (nonatomic,assign) F1MessageItemDirection direction;
 
 - (id) initWithMessage:(NSString*)msg;
 
